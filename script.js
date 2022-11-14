c47adec6855a9543d79227c40b82ee6854c5f100
Devendra <rathaurdevendra2000@gmail.com>
2022-11-14 22:05:49 +0530

    Changes in style.css,loginsignup.html and added jquery

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Welocme to Myntra");
+const slides = document.querySelectorAll(".homeImg");
+// console.log(slides);
+var counter = 0;
+slides.forEach(
+    (slide,index) =>{
+        slide.style.left = `${index* 100}%`
+    }
+)
+
+const goPrev = () => {
+    counter--;
+    slideImage();
+}
+const goNext = () => {
+    counter++;
+    slideImage();
+}
+
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(${counter * 100}%)`
+        }
+    )
+
+    
+}
+
